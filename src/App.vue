8a155f8ad681626947c58c8cf808515d9b18c872
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-08-05 17:21:42 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
@@ -12,6 +12,15 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  computed: {
+    allTodos() {
+      return this.$store.getters.allTodos;
+    }
+  },
+  created() {
+   console.log(this.$store.state);
+   console.log(this.allTodos)
   }
 }
 </script>
