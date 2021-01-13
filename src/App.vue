5c23682e64e9f9f62613c4107d00cbdeb633e520
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-01-12 22:25:10 -0600

    setup my app and most importantly, my vuex store

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
@@ -12,7 +12,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primo;
 }
 
 #nav {
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <StoreTest msg="Welcome to Your Vue.js App" />
+    <StoreTest />
   </div>
 </template>
 
